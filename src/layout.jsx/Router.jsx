8e1cd7ffe57f32e0133f0cd02d8a1e7900f7ebbc
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-04 23:11:01 +0600

    initial setup of the project (tailwind, firebase, react-router-dom etc)

---
+++ src/layout.jsx/Router.jsx
@@ -0,0 +1,11 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world! How are you</div>,
+    },
+  ]);
+  export default router;
