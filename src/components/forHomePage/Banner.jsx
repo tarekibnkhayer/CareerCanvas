ec56bf2256d0d8f086be5213d7841b8cb66997be
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-05 14:12:39 +0600

    [build] -add banner in homepage

---
+++ src/components/forHomePage/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <img src="https://i.imgur.com/XnTA2s0.jpg" alt="" className="min-h-[200px] md:min-h-[400px]" />
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -4,7 +4,7 @@ import {FcAddressBook} from 'react-icons/fc'
 
 const Footer = () => {
     return (
-      <footer className="md:flex mt-10 lg:mt-24 bg-gray-50 items-center">
+      <footer className="md:flex mt-10 lg:mt-24 bg-gray-50 space-y-6 items-center">
         <div className="flex-grow">
         <img src="https://i.imgur.com/xR9jzDk.jpg" alt="website logo" className="w-52 mx-auto"  />
         </div>
@@ -23,7 +23,7 @@ const Footer = () => {
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
     </div>
-    <p className='mt-4 lg:text-xl text-xs'>Copyright © 2023 - All right reserved by <span className='text-blue-800 font-medium lg:font-bold'>CareerCanvas</span> Ltd.</p>
+    <p className='mt-4 lg:text-xl md:text-xs'>Copyright © 2023 - All right reserved by <span className='text-blue-800 font-medium lg:font-bold'>CareerCanvas</span> Ltd.</p>
         </div>
       </footer>
     );
