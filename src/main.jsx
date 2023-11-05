ba5b3014e1a177b9c2994a1a28a6596427f09f13
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-05 20:50:51 +0600

    created PrivateRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './layout.jsx/Router';
+import AuthProviders from './providers/AuthProviders';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+     <AuthProviders>
+     <RouterProvider router={router} />
+     </AuthProviders>
   </React.StrictMode>,
 )
--- src/privateRouter/PrivateRoutes.jsx
+++ src/privateRouter/PrivateRoutes.jsx
@@ -1,11 +1,31 @@
+import { useContext} from "react";
+import { AuthContext } from "../providers/AuthProviders";
+import PropTypes from 'prop-types'
+import { Navigate } from "react-router-dom";
 
-
-const PrivateRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoutes = ({children}) => {
+   const {user, loading} = useContext(AuthContext);
+   if(loading){
+    return <div className="spinner-dot-circle">
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+	<div className="spinner-dot"></div>
+</div>
+   }
+   if(user){
+    return children;
+   }
+   else{
+    return Navigate("/login")
+   }
 };
 
 export default PrivateRoutes;
+PrivateRoutes.propTypes = {
+    children: PropTypes.node
+}
